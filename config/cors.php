8cbd55c293949c8a2b04833cceb816fd110b5feb
Kelvin Sidharta Sie <c14210307@john.petra.ac.id>
2025-03-03 20:07:09 +0700

    feat: add vps - docker - cloudflare cors setting

--- config/cors.php
+++ config/cors.php
@@ -15,27 +15,35 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'users/*'],  // Tambahkan 'users/*'
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'https://slg.petra.ac.id',
-        'http://localhost:5173', // for local development
-        'http://localhost:5174', // for local development
+        'http://localhost:5173',
+        'http://localhost:5174',
         'http://192.168.160.20:5174',
         'http://82.29.165.72:5174',
+        'http://kelvinsidhartasie.my.id:5174',
+        'http://kelvinsidhartasie.my.id',
+        'https://kelvinsidhartasie.my.id',
+        'https://frontend.kelvinsidhartasie.my.id',
     ],
 
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => [
-        'Content-Type',
-        'X-Requested-With',
-        'Authorization',
-        'Accept',
-        'X-XSRF-TOKEN',
+    'allowed_origins_patterns' => [
+        '/^https?:\/\/(.*\.)?kelvinsidhartasie\.my\.id$/i',
     ],
+
+    // 'allowed_headers' => [
+    //     'Content-Type',
+    //     'X-Requested-With',
+    //     'Authorization',
+    //     'Accept',
+    //     'X-XSRF-TOKEN',
+    // ],
+
+    'allowed_headers' => ['*'],  // Debugging
 
     'exposed_headers' => [],
 
